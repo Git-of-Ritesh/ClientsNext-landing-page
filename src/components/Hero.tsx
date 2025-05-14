ff25cb518bb7e4ba2003002672f39ac83a5bc79a
Git-of-Ritesh <rajpandey3942@gmail.com>
2025-05-14 11:12:44 +0530

    added waitlist social avatars

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,6 +3,8 @@ import { Button } from './ui/button'
 import { MoveRight } from 'lucide-react'
 import Shnapshot from "../app/UI frontshot.png"
 import Image from 'next/image'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import Avatar1 from '../app/avatar1.jpg'
 
 const Hero = () => {
     return (
@@ -11,10 +13,32 @@ const Hero = () => {
             <h1 className='text-3xl sm:text-4xl md:text-7xl font-semibold'>One Powerful CRM.</h1>
             <h2 className='mt-6 text-sm sm:text-lg text-center'>Built for freelancers, SaaS developers, and growing businesses — ClientsNext helps you manage leads, </h2>
             <h2 className='sm:text-lg text-sm text-center'>automate workflows, and scale your client relationships without the chaos.</h2>
-            <Button size='xl' className='mt-10 group'>Join waitlist<MoveRight className='ml-1 group-hover:ml-3'/></Button>
-            <div className='mt-24 sm:m-30'>
-            <Image src={Shnapshot} alt="Shnapshot" className='rounded-xl sm:rounded-3xl'/>
-            </div> 
+            <Button size='xl' className='mt-10 group'>Join waitlist<MoveRight className='ml-1 group-hover:ml-3' /></Button>
+            <div className='flex flex-col mt-10 gap-2 sm:flex-row justify-center items-center'>
+            <div className='flex -space-x-2 justify-center items-center'>
+                <Avatar className='ring-2 ring-white'>
+                    <AvatarImage src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?ga=GA1.1.1437547889.1747198823&semt=ais_hybrid&w=740" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <Avatar className='ring-2 ring-white'>
+                    <AvatarImage src="https://img.freepik.com/free-psd/3d-rendering-hair-style-avatar-design_23-2151869153.jpg?ga=GA1.1.1437547889.1747198823&semt=ais_hybrid&w=740" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <Avatar className='ring-2 ring-white'>
+                    <AvatarImage src="https://img.freepik.com/free-psd/3d-illustration-with-online-avatar_23-2151303097.jpg?ga=GA1.1.1437547889.1747198823&semt=ais_hybrid&w=740" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <Avatar className='ring-2 ring-white'>
+                    <AvatarImage src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671116.jpg?ga=GA1.1.1437547889.1747198823&semt=ais_hybrid&w=740" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>      
+            </div>
+            <h1 className='sm:text-xl font-normal text-muted-foreground'>Join 10+ people on the waitlist</h1>
+            </div>
+            
+            <div className='mt-10 sm:mt-28'>
+                <Image src={Shnapshot} alt="Shnapshot" width={1200} height={1200} className='rounded-xl sm:rounded-3xl' />
+            </div>
         </div>
     )
 }
