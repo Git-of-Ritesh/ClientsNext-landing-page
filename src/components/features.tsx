0feee3cf80fde3ce973d5f7a232444ef99323720
Git-of-Ritesh <rajpandey3942@gmail.com>
2025-05-16 01:00:54 +0530

    features section transformed

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -3,6 +3,10 @@ import { Globe } from 'lucide-react'
 import Image from 'next/image'
 import F1Image from "../app/F1image.png"
 import F2Image from "../app/F2image.png"
+import F3Image from "../app/ClientRMimage.png"
+import F4Image from "../app/salesPinelineImage.png"
+import F5Image from "../app/ReminderImage.png"
+import F6Image from '../app/RevenueImage.png'
 
 export default function Features() {
     return (
@@ -10,22 +14,22 @@ export default function Features() {
             <div className="mx-auto max-w-5xl px-6 flex flex-col justify-center items-center">
                 <h1 className='text-lg sm:text-2xl font-semibold '>Features</h1>
                 <h1 className='text-2xl sm:text-5xl font-semibold'>Built to cover your needs</h1>
-                <p className='text-muted-foreground text-sm text-center sm:text-md my-2 mb-18'>Customized CRM experience for your unique journey.</p>
+                <p className='text-muted-foreground text-sm text-center sm:text-lg my-2 mb-18'>Customized CRM experience for your unique journey.</p>
                 <div className="mx-auto grid gap-2 sm:grid-cols-5">
                     <Card className="group overflow-hidden shadow-zinc-950/5 sm:col-span-3 sm:rounded-none sm:rounded-tl-xl">
                         <CardHeader>
                             <div className="md:p-6">
-                                <p className="font-medium">Lead Managment System</p>
-                                <p className="text-muted-foreground mt-3 max-w-sm text-sm">Quick AI lives a single hotkey away - ready to quickly appear as a floating window above your other apps..</p>
+                                <p className="text-xl font-medium">Client Relationship Tracking</p>
+                                <p className="text-muted-foreground mt-3 max-w-sm text-sm">Build stronger connections — track every interaction, note, and milestone to nurture lasting client relationships with ease and clarity.</p>
                             </div>
                         </CardHeader>
 
                         <div className="relative h-full pl-6 md:pl-12">
-                            <div className="absolute -inset-6 [background:radial-gradient(75%_95%_at_50%_0%,transparent,var(--color-background)_100%)]"></div>
+                            {/* <div className="absolute -inset-6 [background:radial-gradient(75%_95%_at_50%_0%,transparent,var(--color-background)_100%)]"></div> */}
 
                             <div className="bg-background overflow-hidden rounded-tl-lg border-l border-t pl-2 pt-2">
                                 <Image
-                                    src={F1Image}
+                                    src={F3Image}
                                     className="shadow dark:hidden"
                                     alt="payments illustration light"
                                     width={657}
@@ -36,22 +40,15 @@ export default function Features() {
                     </Card>
 
                     <Card className="group overflow-hidden shadow-zinc-950/5 sm:col-span-2 sm:rounded-none sm:rounded-tr-xl gap-2">
-                        <p className="mx-auto mt-6 max-w-md text-balance px-6 text-center text-lg font-semibold sm:text-2xl md:pt-6">Advanced UX, Instantly Visualize Sales Pipelines.</p>
-                        <p className='px-6 mx-auto max-w-md text-muted-foreground text-sm text-center'>Organize, prioritize, and close deals faster with visual pipelines.</p>
+                        <p className="mx-auto mt-2 max-w-md text-balance px-6 text-center text-lg font-semibold sm:text-2xl md:pt-4">Advanced UX, Instantly Visualize Sales Pipelines.</p>
+                        <p className='px-6 mb-4 mx-auto max-w-md text-muted-foreground text-sm text-center'>Navigate your deals with clarity — experience seamless, real-time pipeline visualization designed for speed, focus, and powerful sales execution.</p>
 
                         <CardContent className="mt-auto h-fit">
                             <div className="relative mb-6 sm:mb-0">
-                                <div className="absolute -inset-6 [background:radial-gradient(50%_75%_at_75%_50%,transparent,var(--color-background)_100%)]"></div>
+                                {/* <div className="absolute -inset-6 [background:radial-gradient(50%_75%_at_75%_50%,transparent,var(--color-background)_100%)]"></div> */}
                                 <div className="aspect-76/59 overflow-hidden rounded-r-lg border">
                                     <Image
-                                        src="/mail2-light.png"
-                                        className="hidden dark:block"
-                                        alt="payments illustration dark"
-                                        width={1207}
-                                        height={929}
-                                    />
-                                    <Image
-                                        src={F2Image}
+                                        src={F4Image}
                                         className="shadow dark:hidden"
                                         alt="payments illustration light"
                                         width={1207}
@@ -61,58 +58,46 @@ export default function Features() {
                             </div>
                         </CardContent>
                     </Card>
-                    <Card className="group p-6 shadow-zinc-950/5 sm:col-span-2 sm:rounded-none sm:rounded-bl-xl md:p-12">
-                        <p className="mx-auto mb-12 max-w-md text-balance text-center text-lg font-semibold sm:text-2xl">Advanced UX, Instantly locate all your assets.</p>
+                    <Card className="group overflow-hidden shadow-zinc-950/5 sm:col-span-2 sm:rounded-none sm:rounded-tr-xl gap-2">
+                        <p className="mx-auto mt-2 max-w-md text-balance px-6 text-center text-lg font-semibold sm:text-2xl md:pt-4">Smart reminders & follow-ups</p>
+                        <p className='px-6 mb-4 mx-auto max-w-md text-muted-foreground text-sm text-center'>Never miss a beat — stay on top of every client interaction with intelligent reminders and timely, automated follow-ups.</p>
 
-                        <div className="flex justify-center gap-6">
-                            <div className="inset-shadow-sm dark:inset-shadow-white/5 bg-muted/35 relative flex aspect-square size-16 items-center rounded-[7px] border p-3 shadow-lg ring dark:shadow-white/5 dark:ring-black">
-                                <span className="absolute right-2 top-1 block text-sm">fn</span>
-                                <Globe className="mt-auto size-4" />
-                            </div>
-                            <div className="inset-shadow-sm dark:inset-shadow-white/5 bg-muted/35 flex aspect-square size-16 items-center justify-center rounded-[7px] border p-3 shadow-lg ring dark:shadow-white/5 dark:ring-black">
-                                <span>K</span>
-                            </div>
-                        </div>
-                    </Card>
-                    <Card className="group relative shadow-zinc-950/5 sm:col-span-3 sm:rounded-none sm:rounded-br-xl">
-                        <CardHeader className="p-6 md:p-12">
-                            <p className="font-medium">Advanced tracking system</p>
-                            <p className="text-muted-foreground mt-2 max-w-sm text-sm">Quick AI lives a single hotkey away apps..</p>
-                        </CardHeader>
-                        <CardContent className="relative h-fit px-6 pb-6 md:px-12 md:pb-12">
-                            <div className="grid grid-cols-4 gap-2 md:grid-cols-6">
-                                <div className="rounded-(--radius) aspect-square border border-dashed"></div>
-                                <div className="rounded-(--radius) bg-muted/50 flex aspect-square items-center justify-center border p-4">
-                                    <img
-                                        className="m-auto size-8 invert dark:invert-0"
-                                        src="https://oxymor-ns.tailus.io/logos/linear.svg"
-                                        alt="Linear logo"
-                                        width="32"
-                                        height="32"
-                                    />
-                                </div>
-                                <div className="rounded-(--radius) aspect-square border border-dashed"></div>
-                                <div className="rounded-(--radius) bg-muted/50 flex aspect-square items-center justify-center border p-4">
-                                    <img
-                                        className="m-auto size-8 invert dark:invert-0"
-                                        src="https://oxymor-ns.tailus.io/logos/netlify.svg"
-                                        alt="Netlify logo"
-                                        width="32"
-                                        height="32"
-                                    />
-                                </div>
-                                <div className="rounded-(--radius) aspect-square border border-dashed"></div>
-                                <div className="rounded-(--radius) bg-muted/50 flex aspect-square items-center justify-center border p-4">
-                                    <img
-                                        className="m-auto size-8 invert dark:invert-0"
-                                        src="https://oxymor-ns.tailus.io/logos/github.svg"
-                                        alt="github logo"
-                                        width="32"
-                                        height="32"
+                        <CardContent className="mt-auto h-fit">
+                            <div className="relative mb-6 sm:mb-0">
+                                {/* <div className="absolute -inset-6 [background:radial-gradient(50%_75%_at_75%_50%,transparent,var(--color-background)_100%)]"></div> */}
+                                <div className="aspect-76/59 overflow-hidden rounded-r-lg border">
+                                    <Image
+                                        src={F5Image}
+                                        className="shadow dark:hidden"
+                                        alt="payments illustration light"
+                                        width={1207}
+                                        height={929}
                                     />
                                 </div>
                             </div>
                         </CardContent>
+                    </Card>
+                    <Card className="group overflow-hidden shadow-zinc-950/5 sm:col-span-3 sm:rounded-none sm:rounded-tl-xl">
+                        <CardHeader>
+                            <div className="md:p-6">
+                                <p className="text-xl font-medium">Simple analytics dashboard</p>
+                                <p className="text-muted-foreground mt-3 max-w-sm text-sm">Effortlessly monitor performance, track key metrics, and gain instant insights with a clean, intuitive, and customizable analytics dashboard interface.</p>
+                            </div>
+                        </CardHeader>
+
+                        <div className="relative h-full pl-6 md:pl-12">
+                            {/* <div className="absolute -inset-6 [background:radial-gradient(75%_95%_at_50%_0%,transparent,var(--color-background)_100%)]"></div> */}
+
+                            <div className="bg-background overflow-hidden rounded-tl-lg border-l border-t pl-2 pt-2">
+                                <Image
+                                    src={F6Image}
+                                    className="shadow dark:hidden"
+                                    alt="payments illustration light"
+                                    width={657}
+                                    height={929}
+                                />
+                            </div>
+                        </div>
                     </Card>
                 </div>
             </div>
